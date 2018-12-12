9c810de807e36efed82ec75aa5a03c48f749c0a8
Adrian Gomez Brandon <adrian.gbrandon@udc.es>
2018-12-12 11:34:09 +0100

    RCT_dac and RCT

--- src/rct_dac_main.cpp
+++ src/rct_dac_main.cpp
@@ -49,9 +49,9 @@ int main(int argc, const char* argv[]) {
         uint32_t size_reference = (uint32_t) atoi(argv[2]) * 1024*1024;
         uint32_t size_block_bytes = (uint32_t) atoi(argv[3]);
         rct::rct_index<2, rct::log_reference<>, rct::log_object_dac_vector> m_rct_index(argv[1], size_reference, size_block_bytes, 120);
-        std::ofstream out("rct_index_dac" + std::to_string(size_reference) + "_" + std::to_string(size_block_bytes) + ".html");
+        std::ofstream out("rct_index_dac_" + std::to_string(size_reference) + "_" + std::to_string(size_block_bytes) + ".html");
         sdsl::write_structure<sdsl::HTML_FORMAT>(m_rct_index, out);
-        std::string index_file = "rct_index_" + std::to_string(size_reference) + "_" + std::to_string(size_block_bytes) + ".idx";
+        std::string index_file = "rct_index_dac_" + std::to_string(size_reference) + "_" + std::to_string(size_block_bytes) + ".idx";
         sdsl::store_to_file(m_rct_index, index_file);
         sdsl::util::clear(m_rct_index);
         //rct::rct_index<2, rct::log_reference<>, rct::log_object_int_vector> m_rct_index;
