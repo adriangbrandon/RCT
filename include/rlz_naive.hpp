03dc082e1c92964a84d5d1552f8933dbee3c03ca
Adrian Gomez Brandon <adrian.gbrandon@udc.es>
2019-02-02 18:33:26 +0100

    RCT repair

--- include/rlz_naive.hpp
+++ include/rlz_naive.hpp
@@ -58,7 +58,7 @@ namespace rct {
 
         rlz_naive(std::vector<value_type> &container, const size_type reference_size, const size_type block_size){
 
-            m_reference = reference_type(container);
+            m_reference = reference_type(container, reference_size, block_size);
             sdsl::int_vector<> rev_reference;
             //std::vector<uint32_t> rev_reference;
             // rev_reference.width((uint8_t) (sizeof(value_type)*8));
